b1c6117306a6c38686b394c4d87d4a51441e1bd3
Gaurav <109208028+gauravvvvvvvvvv@users.noreply.github.com>
2024-07-07 15:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import tempfile
+import av
 
 # Import your YOLO_Pred class here
 from yolo_predictions import YOLO_Pred
@@ -21,44 +22,21 @@ def process_video(video_file, model):
     tfile = tempfile.NamedTemporaryFile(delete=False) 
     tfile.write(video_file.read())
 
-    vf = cv2.VideoCapture(tfile.name)
-    stframe = st.empty()
-    while vf.isOpened():
-        ret, frame = vf.read()
-        if not ret:
-            break
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result_rgb = process_image(frame_rgb, model)
-        stframe.image(result_rgb)
-        
-    vf.release()
-
-def process_camera(model):
-    cap = cv2.VideoCapture(0)
+    container = av.open(tfile.name)
     stframe = st.empty()
 
-    while st.session_state.camera_on:
-        ret, frame = cap.read()
-        if not ret:
-            st.error("Failed to capture image from camera.")
-            break
-        
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result_rgb = process_image(frame_rgb, model)
-        
+    for frame in container.decode_video():
+        img = cv2.cvtColor(np.array(frame.to_ndarray(format="bgr24")), cv2.COLOR_BGR2RGB)
+        result_rgb = process_image(Image.fromarray(img), model)
         stframe.image(result_rgb)
-    
-    cap.release()
+
+    st.text("Video processing complete!")
 
 def main():
     st.title("Object Detection with YOLO")
 
     # Load the YOLO model
     model = load_model()
-
-    # Initialize session state
-    if 'camera_on' not in st.session_state:
-        st.session_state.camera_on = False
 
     # Input type selection
     input_type = st.radio("Select input type:", ("Image", "Video", "Camera"))
@@ -76,19 +54,15 @@ def main():
         uploaded_file = st.file_uploader("Choose a video...", type=["mp4", "avi", "mov"])
         if uploaded_file is not None:
             process_video(uploaded_file, model)
-            st.text("Video processing complete!")
     
     elif input_type == "Camera":
         st.write("Click the button to start or stop the camera.")
         
         # Single button to toggle camera state
-        if st.button('Toggle Camera' if not st.session_state.camera_on else 'Stop Camera'):
-            st.session_state.camera_on = not st.session_state.camera_on
-            
-            if st.session_state.camera_on:
-                process_camera(model)
-            else:
-                st.experimental_rerun()
+        if st.button('Toggle Camera'):
+            st.write("Camera is on. Please wait...")
+            process_camera(model)
+            st.write("Camera is off.")
 
 if __name__ == "__main__":
     main()
